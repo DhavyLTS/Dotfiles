f7c05ce6196af6f768dd29cf6d97361de6341367
DhavyLTS <dhavy.s@proton.me>
2024-07-10 02:08:12 -0300

    init: lazy plugin manager

---
+++ .config/nvim/lua/core/lazy.lua
@@ -0,0 +1,14 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	ui = { wrap = false },
+  checker = { enabled = true },
+  spec = { { import = "plugins" } },
+  install = { colorscheme = { "kanagawa" } },
+})
