bda56aeb3f9e95e06371da546670b2e0b2aa9788
DhavyLTS <dhavy.s@proton.me>
2024-07-17 22:03:49 -0300

    init: install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if !command -v yay &> /dev/null; then
+	git clone https://aur.archlinux.org/yay-git /tmp/yay-git
+	cd /tmp/yay-git && makepkg -si --noconfirm
+	yay -S --noconfirm $(<packages.txt)
+fi
+
+if ! [[ -d $HOME/.oh-my-zsh ]]; then
+	git clone https://github.com/ohmyzsh/ohmyzsh $HOME/.oh-my-zsh
+	git clone https://github.com/zplug/zplug $HOME/.zplug
+	sudo chsh -s $(which zsh) $(whoami)
+fi
+
+if !command -v cargo &> /dev/null; then
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -- -y && source $HOME/.cargo/env
+	cargo install xremap --features x11
+	cargo install bat bacon
+fi
+
+if ! [[ -f $HOME/.stowed ]]; then
+	cd $HOME/.dotfiles && stow .
+fi
